b66457a1825fe84ecd9fdc1a2030cca84fde38ef
66redBastard <66redbastard@gmail.com>
2021-08-11 04:59:07 +0300

    FSA-1: signUp refactored, working fine, login refactoring WIP

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,3 +1,42 @@
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const secretToken = process.env.JWT_SECRET;
+
+// handle errors
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: "", password: "" };
+
+  // incorrect email
+  if (err.message === "incorrect email") {
+    errors.email = "That email is not registered";
+  }
+
+  // incorrect password
+  if (err.message === "incorrect password") {
+    errors.password = "That password is incorrect";
+  }
+
+  // duplicate email error
+  if (err.code === 11000) {
+    errors.email = "that email is already registered";
+    return errors;
+  }
+
+  // validation errors
+  if (err.message.includes("user validation failed")) {
+    // console.log(err);
+    Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(val);
+      // console.log(properties);
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
+
 module.exports.signupGet = (req, res) => {
   res.render("signup");
 };
@@ -6,9 +45,52 @@ module.exports.loginGet = (req, res) => {
   res.render("login");
 };
 
-module.exports.signupPost = (req, res) => {
-  res.status(200).send("signup");
+module.exports.signupPost = async (req, res) => {
+  try {
+    // Get user input
+    const { first_name, last_name, email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && first_name && last_name)) {
+      return res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+    console.log(password, email, first_name, last_name);
+
+    //Encrypt user password
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign({ user_id: user._id, email }, secretToken, {
+      expiresIn: "6h",
+    });
+    // save user token
+    user.token = token;
+
+    // return new user
+    return res.status(201).json(user);
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
 };
+
+// actions
 module.exports.loginPost = (req, res) => {
   res.status(200).send("signup");
 };
--- models/User.js
+++ models/User.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
   first_name: { type: String, default: null },
   last_name: { type: String, default: null },
-  email: { type: String, unique: true },
-  password: { type: String },
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: [6, "Minimum password length is 6 chars"],
+  },
   // token: { type: String },
 });
 
