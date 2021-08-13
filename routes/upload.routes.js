92667864d4c1901c217f5cc95681fdf96ac1d4ee
66redBastard <66redbastard@gmail.com>
2021-08-13 03:56:43 +0300

    FSA-7: uploading file to AWS s3 failed in cause of AuthorizationHeaderMalformed

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,23 +1,50 @@
+require("dotenv").config();
 const { Router } = require("express");
 const multer = require("multer");
-// const upload = multer({ dest: "../public/files" });
-const upload = multer();
+const upload = multer({ dest: "../public/files" });
+// const upload = multer();
 const jwt = require("jsonwebtoken");
 const secretToken = process.env.JWT_SECRET;
+const fs = require("fs");
+const aws = require("aws-sdk");
 
-// const upload = require("../controller/upload.controller");
+aws.config.update({
+  apiVersion: "2006-03-01",
+  secretAccessKey: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION,
+});
+const s3 = new aws.S3();
 
 const uploadRoutes = Router();
 
-uploadRoutes.post("/uploadFile", upload.single("uploadFile"), (req, res) => {
-  //   console.log(req.user); TODO: WHY dis NOT WORKIN
-  console.log(req.file);
-  const token = req.headers.cookie.substring(4);
-  const decodedToken = jwt.verify(token, secretToken);
-  console.log("decoded", decodedToken);
+uploadRoutes.post(
+  "/uploadFile",
+  upload.single("uploadFile"),
+  async (req, res) => {
+    //   console.log(req.user); TODO: WHY dis NOT WORKIN
+    console.log(req.file);
+    const token = req.headers.cookie.substring(4);
+    const decodedToken = jwt.verify(token, secretToken);
+    console.log("decoded", decodedToken);
+    const fileSream = fs.createReadStream(req.file.path);
 
-  return res.status(200);
-});
+    const uploadParams = {
+      Bucket: process.env.AWS_BUCKET,
+      Body: fileSream,
+      Key: req.file.filename,
+    };
+
+    try {
+      console.log();
+      const result = await s3.upload(uploadParams).promise();
+      return res.status(200).json({ result });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({ err });
+    }
+  }
+);
 
 // uploadRoutes.post("/uploadFile", upload.multiple("uploadFile"), (req, res) => {
 //   console.log(req.file);
